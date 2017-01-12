cd21e71fb2e15704ea02159085ac0c6f1977a256
RaoHai <surgesoft@gmail.com>
2017-01-12 19:29:20 +0800

    add scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-npm run build
-gulp
+
+# include parse_yaml functio
+. scripts/parse_yaml.sh
+
+# npm run build
+# gulp
+
+cp -r scaffolds/* out/
+cd out
+
+for D in *; do
+    if [ -d "${D}" ]; then
+        echo "${D}"   # your processing here
+        cd $D
+        git diff ./ | wc -l
+        # eval $(parse_yaml index.yml "config_")
+        # REPO=$(echo ${config_git_url} | sed "s/'//g")
+        # echo $REPO
+        # git clone $REPO src
+        # cd src
+        # npm install
+        cd ..
+    fi
+done
