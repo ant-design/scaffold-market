7c0798bb74cea537ea19ab73873fdcd725d73b7f
RaoHai <surgesoft@gmail.com>
2017-01-13 17:20:13 +0800

    lalala

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,13 +8,6 @@ function doCompile {
   ./scripts/compile.sh
 }
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
-
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
@@ -29,6 +22,13 @@ cd ..
 
 # Clean out existing contents
 rm -rf out/**/* || exit 0
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
 
 # Run our compile script
 doCompile
