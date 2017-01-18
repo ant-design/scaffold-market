b5dd11cc86429f9fa9d40d38c77a12c350e725c9
RaoHai <surgesoft@gmail.com>
2017-01-18 10:51:31 +0800

    update domain

--- src/services/list.js
+++ src/services/list.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export function fetch() {
-  return axios.get('https://ant-design.github.io/scaffold-market/list.json');
+  return axios.get('http://scaffold.ant.design/list.json');
 }
