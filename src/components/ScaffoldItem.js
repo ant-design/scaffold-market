5bccd8f8c0670eafcac8af2f435e309d80724258
afc163 <afc163@gmail.com>
2017-03-20 14:06:51 +0800

    update code style

--- src/components/ScaffoldItem.js
+++ src/components/ScaffoldItem.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Icon, Button } from 'antd';
 
 export default function ScaffoldItem({ data, styles }) {
-  return (<li>
-    <article>
-      <header>{data.name}[{data.chineseName}]
-        <a href={data.source}><Icon type="github" /></a>
-      </header>
-      <div>{data.description}</div>
-      <div className={styles.actions}>
-        <Button.Group>
-          <Button><a href={`${data.name}`} target="_blank"> DEMO </a></Button>
-          <Button> Open in IDE</Button>
-        </Button.Group>
-      </div>
-    </article>
-  </li>);
+  return (
+    <li>
+      <article>
+        <header>{data.name}[{data.chineseName}]
+          <a href={data.source}><Icon type="github" /></a>
+        </header>
+        <div>{data.description}</div>
+        <div className={styles.actions}>
+          <Button.Group>
+            <Button>
+              <a href={`${data.name}`} target="_blank" rel="noopener noreferrer">
+                DEMO
+              </a>
+            </Button>
+            <Button>
+              Open in IDE
+            </Button>
+          </Button.Group>
+        </div>
+      </article>
+    </li>
+  );
 }
