2206252d62909be1f8d48f45fd024fc29e66ece1
afc163 <afc163@gmail.com>
2017-04-07 17:11:27 +0800

    Add scaffold page

--- src/routes/ScaffoldPage.js
+++ src/routes/ScaffoldPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Card, Layout, Spin } from 'antd';
 import styles from './ScaffoldPage.less';
 
+const { Sider, Content } = Layout;
+
 const ScaffoldPage = ({ list, params }) => {
-  const scaffold = list.filter(item => item.name === params.templateId);
+  const scaffold = list.filter(item => item.name === params.templateId)[0];
+  if (!scaffold) {
+    return <Spin size="large" />;
+  }
   return (
     <div className={styles.main}>
-      {JSON.stringify(scaffold)}
+      <Layout>
+        <Sider style={{ background: 'transparent' }}>
+          {scaffold.name}
+        </Sider>
+        <Content>
+          <Card title="README">
+            content
+          </Card>
+        </Content>
+      </Layout>
     </div>
   );
 };
