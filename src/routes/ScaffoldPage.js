02ff6b7cccf67d83ae9b4e8ecb5dd26bf0bfcd6e
afc163 <afc163@gmail.com>
2017-04-10 16:35:28 +0800

    Scaffold page style

--- src/routes/ScaffoldPage.js
+++ src/routes/ScaffoldPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Card, Layout, Spin } from 'antd';
+import { Card, Layout, Spin, Icon } from 'antd';
 import ReactMarkdown from 'react-markdown';
 import styles from './ScaffoldPage.less';
 
@@ -17,12 +17,29 @@ const ScaffoldPage = ({ list, params }) => {
   }
   return (
     <Layout>
-      <Sider style={{ background: 'transparent' }} width="400">
-        <h2>{scaffold.name}</h2>
+      <Sider className={styles.sider} width={400}>
+        <div className={styles.header}>
+          <h2 title={scaffold.name}>{scaffold.name}</h2>
+          <div className={styles.right}>
+            <Icon type="star" className={styles.star} />{scaffold.stargazers_count}
+          </div>
+        </div>
+        <p>{scaffold.description}</p>
+        <hr />
+        <p>
+          <h3>基本信息</h3>
+          <span>提交人：{scaffold.author}</span>
+          <span>提交时间：{scaffold.createdAt}</span>
+          <span>更新时间：{scaffold.updatedAt}</span>
+        </p>
+        <hr />
+        <p>
+          <h3>标签</h3>
+        </p>
       </Sider>
       <Content>
-        <Card title="README">
-          <ReactMarkdown source={scaffold.readme} />
+        <Card title="README" style={{ border: '1px solid #e9e9e9' }}>
+          <ReactMarkdown source={scaffold.readme} className={styles.markdown} />
         </Card>
       </Content>
     </Layout>
