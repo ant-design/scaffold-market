ed82af7d60ab59e754d2ec54d49206f028cdc598
afc163 <afc163@gmail.com>
2017-04-12 16:49:07 +0800

    use prop-types

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
 import { Spin, Layout, Row, Col, Tag } from 'antd';
