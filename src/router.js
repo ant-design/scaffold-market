3465f5bb8af4c903ca311aedfb86bf96d13d08f0
afc163 <afc163@gmail.com>
2017-05-03 17:01:12 +0800

    Add react-ga

--- src/router.js
+++ src/router.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Router, Route, IndexRoute } from 'dva/router';
+import ReactGA from 'react-ga';
 import IntlCommonLayout from './Layout/IntlCommonLayout';
 import IndexPage from './routes/IndexPage';
 import Contribute from './routes/Contribute';
 import ScaffoldPage from './routes/ScaffoldPage';
 
+ReactGA.initialize('UA-72788897-4');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
+    <Router history={history} onUpdate={logPageView}>
       <Route path="/" component={IntlCommonLayout}>
         <IndexRoute component={IndexPage} />
         <Route path="contribute" component={Contribute}>
