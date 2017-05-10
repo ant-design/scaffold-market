40f8e800272fe6ef292d40df66bbcd586984be42
afc163 <afc163@gmail.com>
2017-05-10 17:04:56 +0800

    Add placeholder cover picture

--- src/components/ScaffoldItem.js
+++ src/components/ScaffoldItem.js
@@ -5,11 +5,12 @@ import Overdrive from 'react-overdrive';
 import styles from './ScaffoldItem.less';
 
 export default function ScaffoldItem({ name, description, stargazers_count, coverPicture }) {
+  const picture = coverPicture || `https://placeholdit.imgix.net/~text?txtsize=60&bg=ed33ba&txtclr=ffffff&txt=${name}&w=560&h=360&txttrack=0`;
   return (
     <li className={styles.card}>
       <Link to={`/scaffolds/${name}`}>
         <Overdrive id={`cover-${name}`} duration={400}>
-          <img className={styles.picture} src={coverPicture} alt="" />
+          <img className={styles.picture} src={picture} alt="" />
         </Overdrive>
         <div className={styles.detail}>
           <h2 className={styles.title}>
