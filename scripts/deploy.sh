421ff8d865b2956ad751c58ee2f48d5305c8e5f5
afc163 <afc163@gmail.com>
2018-05-14 11:47:20 +0800

    improve clone

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
+git clone $REPO out --depth=1
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 echo "clean out non scaffolds files from gh-pages"
